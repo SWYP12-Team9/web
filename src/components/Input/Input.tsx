f4a71e5bb35d4eb79d56d7da5f2a8d1aaaa7abc7
hoon <dudrn587@gmail.com>
2026-01-31 21:55:34 +0900

    feat: input 공통 컴포넌트 prop 추가

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,13 +2,22 @@ import { cn } from '@/src/utils/cn'
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string
+  width?: string
+  height?: string
 }
 
-export function Input({ className, ...props }: InputProps) {
+export function Input({
+  className,
+  width = 'w-full',
+  height,
+  ...props
+}: InputProps) {
   return (
     <input
       className={cn(
-        'rounded-8 placeholder:text-gray-muted bg-gray-field text-gray-default text-caption-1 w-full px-20 py-14',
+        'rounded-8 placeholder:text-gray-muted bg-gray-field text-gray-default text-caption-1 px-20 py-14',
+        width,
+        height,
         className,
       )}
       {...props}
