4986e3b3f3f0ada0ba66193658db7fb8dd6bc24d
hoon <dudrn587@gmail.com>
2026-02-02 18:00:40 +0900

    feat: Sidebar 레이아웃에 배치

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import { Sidebar } from '@/src/components/Sidebar'
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="bg-blue-light flex min-h-screen">
+      <Sidebar isExpanded={true} />
+
+      <main className="relative flex-1 pt-20">
+        <div className="rounded-tl-20 min-h-full bg-white">{children}</div>
+      </main>
+    </div>
+  )
+}
--- src/components/Icon/NavHome.tsx
+++ src/components/Icon/NavHome.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import * as React from 'react'
 import type { SVGProps } from 'react'
 const SvgNavHome = (props: SVGProps<SVGSVGElement>) => (
