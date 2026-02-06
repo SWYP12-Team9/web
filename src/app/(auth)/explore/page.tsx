3252685f99a23b0d14fbbab328f677315d3d810a
hoon <dudrn587@gmail.com>
2026-02-06 11:55:15 +0900

    feat: 카테고리 목록 tabs ui 추가

--- src/app/(auth)/explore/page.tsx
+++ src/app/(auth)/explore/page.tsx
@@ -1,11 +1,33 @@
 'use client'
 
+import { useGetCategories } from '@/src/apis/query/recommendation/useGetCategories'
 import { ProfileSetup } from './_components/ProfileSetup'
+import { Tab, Tabs } from '@/src/components/Tabs'
+import { ALL_TAB } from '@/src/constants/defaultTap'
+import { useState } from 'react'
 
 export default function ExplorePage() {
+  const [selectedTab, setSelectedTab] = useState<Tab>(ALL_TAB)
+
+  const { data: categories } = useGetCategories()
+
+  const tabs = categories?.data.map((category, index) => ({
+    id: index,
+    title: category,
+  }))
+
+  const handleTabChange = (tab: Tab) => {
+    setSelectedTab(tab)
+  }
+
   return (
     <main>
-      <h1>탐색 페이지</h1>
+      <Tabs
+        defaultTap={ALL_TAB}
+        tabs={tabs}
+        selectedTab={selectedTab}
+        onChange={handleTabChange}
+      />
       <ProfileSetup />
     </main>
   )
