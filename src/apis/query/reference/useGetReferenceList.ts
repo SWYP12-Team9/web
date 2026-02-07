08c0344da0f2de49b431178c163acc544d23cfef
hyeon-aa <ahhyun1120@naver.com>
2026-02-08 00:14:14 +0900

    feat: 레퍼런스 리스트 조회

--- src/apis/query/reference/useGetReferenceList.ts
+++ src/apis/query/reference/useGetReferenceList.ts
@@ -1,16 +1,32 @@
-import { useQuery } from '@tanstack/react-query'
-import {
-  requestGetReferenceList,
-  RequestGetReferenceListParams,
-} from '../../request/requestGetReferenceList'
+import { ReferenceVisibility } from '@/src/types/reference/reference'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { requestGetReferenceList } from '../../request/requestGetReferenceList'
 import { referenceKeys } from './referenceKeys'
+
+interface UseGetReferenceListParams {
+  type: ReferenceVisibility
+  size?: number
+}
 
 export const useGetReferenceList = ({
   type,
-  sortBy,
-}: RequestGetReferenceListParams) => {
-  return useQuery({
-    queryKey: referenceKeys.list(type),
-    queryFn: () => requestGetReferenceList({ type }),
+  size = 20,
+}: UseGetReferenceListParams) => {
+  return useInfiniteQuery({
+    queryKey: referenceKeys.listInfinite({ type, size }),
+
+    queryFn: ({ pageParam }) =>
+      requestGetReferenceList({
+        type,
+        cursor: pageParam,
+        size,
+      }),
+
+    initialPageParam: null as string | null,
+
+    getNextPageParam: (lastPage) => {
+      const { hasNext, nextCursor } = lastPage.data
+      return hasNext ? nextCursor : null
+    },
   })
 }
