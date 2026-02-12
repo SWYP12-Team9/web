f8a8d88360e74c375ce34d15109937dbe590c0e9
hyeon-aa <ahhyun1120@naver.com>
2026-02-13 01:16:26 +0900

    fix: 마이페이지 플로팅 버튼 제거

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,18 @@
+'use client'
+
 import { SaveLinkModal } from '@/src/components/Modal'
 import { OpenSaveLinkButton } from '@/src/components/OpenSaveLinkButton/OpenSaveLinkButton'
 import { Sidebar } from '@/src/components/Sidebar'
+import { usePathname } from 'next/navigation'
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
+  const isMyPage = pathname === '/mypage'
+
   return (
     <div className="bg-blue-light flex h-screen">
       <Sidebar />
@@ -15,7 +21,7 @@ export default function AuthLayout({
         <div className="rounded-tl-20 flex min-w-0 flex-1 flex-col bg-white">
           {children}
         </div>
-        <OpenSaveLinkButton />
+        {!isMyPage && <OpenSaveLinkButton />}
         <SaveLinkModal />
       </main>
     </div>
